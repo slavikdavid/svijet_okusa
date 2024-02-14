0354a368050b9a7e23294373f486c92f3f9705cb
David Slavik <slavikdavid@protonmail.com>
2024-02-14 21:21:31 +0100

    Render Navbar on every route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { Link, Route, Routes} from "react-router-dom";
 
 const Navbar = () => {
-    return <div>
+    return (
+        <div>
         <h1>Navbar</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/recipe'>Recipe (individual)</Link>
-        <Link to='/recipes'>Recipes</Link>
-        <Link to='/ingredient'>Ingredient (individual)</Link>
-        <Link to='/ingredients'>Ingredients</Link>
-        <Link to='/login'>Login</Link>
+        <p>
+        <Link to="/">Home</Link>
+        </p>
+        <p>
+        <Link to="/recipe">Recipe (individual)</Link>
+        </p>
+        <p>
+        <Link to="/recipes">Recipes</Link>
+        </p>
+        <p>
+        <Link to="/ingredient">Ingredient (individual)</Link>
+        </p>
+        <p>
+        <Link to="/ingredients">Ingredients</Link>
+        </p>
+        <p>
+        <Link to="/login">Login</Link>
+        </p>
     </div>
+    )
 }
 
 export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomePage from './routes/HomePage.jsx'
-import RecipePage from './routes/RecipePage.jsx'
-import RecipesPage from './routes/RecipesPage.jsx'
-import IngredientPage from './routes/IngredientPage.jsx'
-import IngredientsPage from './routes/IngredientsPage.jsx'
-import LoginPage from './routes/LoginPage.jsx'
-
-const router = createBrowserRouter(
-  [
-    { path: "/", element: <HomePage />},
-    { path: "/recipe", element: <RecipePage />},
-    { path: "/recipes", element: <RecipesPage />},
-    { path: "/ingredient", element: <IngredientPage />},
-    { path: "/ingredients", element: <IngredientsPage />},
-    { path: "/login", element: <LoginPage />},
-  ]
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './routes/HomePage';
+import RecipePage from './routes/RecipePage';
+import RecipesPage from './routes/RecipesPage';
+import IngredientPage from './routes/IngredientPage';
+import IngredientsPage from './routes/IngredientsPage';
+import LoginPage from './routes/LoginPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
+<React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe" element={<RecipePage />} />
+        <Route path="/recipes" element={<RecipesPage />} />
+        <Route path="/ingredient" element={<IngredientPage />} />
+        <Route path="/ingredients" element={<IngredientsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
