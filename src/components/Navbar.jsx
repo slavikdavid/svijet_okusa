adc559b3d472251119dda8845ae2e2c210941cd6
David Slavik <slavikdavid@protonmail.com>
2024-02-15 01:05:18 +0100

    Change 'sl' value to 'si' to match ISO3166 country code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
                   >
                     {getUnicodeFlagIcon("GB")} English
                   </MenuItem>
-                  <MenuItem value="sl">
+                  <MenuItem value="si">
                     <Text fontSize="">
                       {getUnicodeFlagIcon("SI")} Slovenian
                     </Text>
