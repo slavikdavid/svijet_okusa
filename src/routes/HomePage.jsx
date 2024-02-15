00f3a74eedd1773c553baf5fe019e951325989ee
David Slavik <slavikdavid@protonmail.com>
2024-02-15 03:31:56 +0100

    Add Searchbar and Dropdown menus to Homepage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,11 +1,72 @@
 import { useTranslation } from "react-i18next";
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Heading,
+  HStack,
+  Menu,
+  Flex,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import {
+  SearchIcon,
+  CarrotIcon,
+  TimerIcon,
+  SlidersHorizontalIcon,
+} from "lucide-react";
 
 const HomePage = () => {
   const { t } = useTranslation();
   return (
     <>
-      <Heading>{t("homepage")}</Heading>
+      <Container p={5}>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="black" />
+          </InputLeftElement>
+          <Input placeholder="Enter search prompt..." />
+          <InputRightElement width="4.5rem">
+            <Button colorScheme="red">Search</Button>
+          </InputRightElement>
+        </InputGroup>
+        <Flex justify="space-between" py={15}>
+          <Menu>
+            <MenuButton as={Button}>
+              <CarrotIcon />
+              Ingredients
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+            </MenuList>
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button}>
+              <TimerIcon />
+              Preparation time
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+            </MenuList>
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button}>
+              <SlidersHorizontalIcon />
+              Advanced Search
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Container>
     </>
   );
 };
